62a399b8323c9bac429ae3f81a0eb7365e0060ea
Charlie Tyler <charlietyler@live.com>
2018-02-15 18:16:45 +0000

    reverted

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.autoload_paths << Rails.root.join('lib')
-
 module Bandb
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
---
+++ spec/factories/bookings.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :booking do
+    name "MyString"
+    start_time "2018-02-14 18:03:12"
+    end_time "2018-02-14 18:03:12"
+  end
+end
